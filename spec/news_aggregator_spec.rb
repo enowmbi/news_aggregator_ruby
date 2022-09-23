0bde42eae2b024778fe8cf6b7e5157fc10903300
Enow Mbi <benowmbi@yahoo.com>
2022-09-23 18:14:37 +0100

    Fix broken spec -- require version.rb in spec file

--- spec/news_aggregator_spec.rb
+++ spec/news_aggregator_spec.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require_relative "../lib/news_aggregator/version"
+
 RSpec.describe NewsAggregator do
   it "has a version number" do
     expect(NewsAggregator::VERSION).not_to be nil
