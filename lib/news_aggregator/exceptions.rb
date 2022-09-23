e9c72fc0e880869e466281e4c40475d9c6e724ec
Enow Mbi <benowmbi@yahoo.com>
2022-09-23 12:59:13 +0100

    Move exception class to lib/news_aggregator/

---
+++ lib/news_aggregator/exceptions.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+module NewsAggregator
+  # UnauthorizedException
+  class UnauthorizedException < StandardError
+    def initialize(json)
+      puts json["message"]
+    end
+  end
+
+  # BadRequestException
+  class BadRequestException < StandardError
+    def initialize(json)
+      puts json["message"]
+    end
+  end
+
+  # TooManyRequestException
+  class TooManyRequestsException < StandardError
+    def initialize(json)
+      puts json["message"]
+    end
+  end
+
+  # ServerException
+  class ServerException < StandardError
+    def initialize(json)
+      puts json["message"]
+    end
+  end
+end
