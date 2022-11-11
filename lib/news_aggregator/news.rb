867388a18fbb9de12f5f945a9445b693df53ed7a
Enow Mbi <benowmbi@yahoo.com>
2022-11-11 21:26:34 +0100

    Remove redundant classes

--- lib/news_aggregator/news.rb
+++ lib/news_aggregator/news.rb
@@ -35,13 +35,7 @@ module NewsAggregator
     def sources(**args)
       endpoint = "sources"
       request = _make_request(endpoint, **args)
-      # status = request["status"]
-      sources = request["sources"]
-      data = []
-      sources.each do |source|
-        data.push(create_new_source(source))
-      end
-      data
+      request.data
     end
 
     private
@@ -63,7 +57,6 @@ module NewsAggregator
       when "401" then raise UnauthorizedException, response.body
       when "402" then raise PaymentRequiredException, response.body
       when "403" then raise ForbiddenException, response.body
-      # when "426" then raise TooManyRequestsException, response.body
       when "429" then raise TooManyRequestsException, response.body
       when "500" then raise ServerException, response.body
       end
@@ -71,32 +64,7 @@ module NewsAggregator
 
     def _get_everything(endpoint, **args)
       request = _make_request(endpoint, **args)
-      # status = request['status']
-      # totalResults = request['totalResults']
-      articles = request["articles"]
-      data = []
-      articles.each do |article|
-        data.push(create_everything(article))
-      end
-      data
-    end
-
-    def create_new_source(source)
-      Struct.new("Source", :id, :name, :description, :url, :category, :language, :country)
-      Struct::Source.new(
-        source["id"], source["name"], source["description"],
-        source["url"], source["category"], source["language"],
-        source["country"]
-      )
-    end
-
-    def create_everything(article)
-      Struct.new("Everything", :source, :author, :title, :description, :url, :url_to_image, :published_at, :content)
-      Struct::Everything.new(
-        article["source"], article["author"], article["title"],
-        article["description"], article["content"], article["url"],
-        article["urlToImage"], article["publishedAt"]
-      )
+      request.data
     end
   end
 end
